683f623c2e8cc1f47017f47b08f2cf65d8fc6c63
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-02-27 10:18:24 +0100

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+galleryItems.forEach((item) => {
+    
+    const markup = `<div><img src="${item.preview}" alt="${item.description}"width ="372px" height="240"></div>`;
+    gallery.innerHTML += markup;
+
+})
+
+const originalImage = () => {
+    
+    gallery.innerHTML = `<div><img src="${item.original}" alt="${item.description}"width ="372px" height="240"></div>`;
+
+}
+gallery.addEventListener('click',originalImage)
+console.log(gallery)
 console.log(galleryItems);
